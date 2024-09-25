b36eb9d93de526eeb45b46063e71426a37a61d2d
zhuddan <yobaci@163.com>
2024-09-25 11:58:55 +0800

    2

--- assets/detail-CQG9BvlD.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-01Y-Yu1K.js";function e(){return r.jsx(r.Fragment,{children:"订单详情"})}export{e as default};
--- assets/index-DSGTe9-J.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-01Y-Yu1K.js";function t(){return r.jsx(r.Fragment,{children:"订单列表"})}export{t as default};
